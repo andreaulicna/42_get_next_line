70a3a66a8daf29cb5b9d765dee4324081f14a2f2
Andrea Uličná <106449030+andreaulicna@users.noreply.github.com>
2023-06-10 19:49:34 +0200

    Complete get next line added from school PC

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aulicna <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 11:26:26 by aulicna           #+#    #+#             */
+/*   Updated: 2023/01/28 16:54:13 by aulicna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+// Main functions
+char	*get_next_line(int fd);
+char	*read_fd(int fd, char *s);
+char	*process_current_line(char *s);
+char	*save_remainder_of_line(char *s);
+
+// Utility functions
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
